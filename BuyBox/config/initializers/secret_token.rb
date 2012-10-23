b31e9d00a1c6086d8802f68686b2cf1628e63651
Arjun Patel <arjunap@gmail.com>
2012-10-22 17:55:25 -0700

    Initial Commit - Project Setup

---
+++ BuyBox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyBox::Application.config.secret_token = '0db2fb34e38a31fb4351adabfa125ed409ec29de347076a048fd67b3fad71d001ac2ef9ad17efcbc67b18fa4236432b63c9a17af22bed117fd5963f743759227'
